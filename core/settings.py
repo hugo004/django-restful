c4bce4795b270c8d3777a8a136f3d7c9cfe5a84a
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2021-06-26 15:40:07 +0000

    rename folder

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
